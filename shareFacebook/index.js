e9779e3b4030a2fb20f7741238ee2706d30810d8
Laud Bentil <laud.bentil@meltwater.org>
2016-12-23 07:07:29 +0000

    remove unneeded files

--- shareFacebook/index.js
+++ shareFacebook/index.js
@@ -4,14 +4,14 @@
 
 const FB = require('fb');
 
-const access_token; // should come from a locally creted file
+const access_token;
 
 exports.shareToFacebook = function (values) {
     const msg = values.msg;
     const page_id = values.page_id;
 
     FB.setAccessToken(access_token);
-    // var body = 'been a while peeps';
+    
     FB.api('/'+page_id+'/feed', 'post', {message: msg }, function (res) {
       if(!res || res.error) {
         console.log( !res ? 'error occurred' : res.error );
